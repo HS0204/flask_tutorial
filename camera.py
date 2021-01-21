3a05c81b0da7806ad814a09a79175440379eab63
Hanseul Lee <lhanseul0204@naver.com>
2021-01-21 22:16:43 +0900

    add face detection

--- camera.py
+++ camera.py
@@ -5,8 +5,16 @@ class VideoCamera(object):
     def __init__(self):
         self.stream = WebcamVideoStream(src=0).start()
 
+    def __del__(self):
+        self.stream.stop()
+
     def get_frame(self):
         image = self.stream.read()
+
+        detector = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+        face = detector.detectMultiScale(image, 1.1, 7)
+        for (x,y,h,w) in face:
+            cv.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 2)
 
         ret, jpeg = cv.imencode('.jpg', image)
         data = []
